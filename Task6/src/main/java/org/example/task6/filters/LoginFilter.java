e1cc3d4f2cbcc1bcc6f686f9ae68be49a576257a
Vlad <prokopchenko@mail.ru>
2025-03-04 17:21:55 +0300

    добавил фильтр

--- Task6/src/main/java/org/example/task6/filters/LoginFilter.java
+++ Task6/src/main/java/org/example/task6/filters/LoginFilter.java
@@ -1,12 +1,25 @@
 package org.example.task6.filters;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
 public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = ((HttpServletRequest) servletRequest).getSession();
+        boolean loggedIn = (session != null && session.getAttribute("isLoggedIn") == Boolean.TRUE );
 
+        if (loggedIn) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+        else {
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
     }
 }
--- Task6/src/main/java/org/example/task6/servlets/EditServlet.java
+++ Task6/src/main/java/org/example/task6/servlets/EditServlet.java
@@ -1,5 +1,6 @@
 package org.example.task6.servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,8 +10,9 @@ import java.io.IOException;
 
 public class EditServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/edit.jsp");
+        rd.forward(request, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
